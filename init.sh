f0fffcf6b26665f6d2e1e598af6430a0dc78442c
toprak1212 <128628316+toprak1212@users.noreply.github.com>
2023-04-08 04:24:27 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ wget https://raw.githubusercontent.com/toprak1212/tmp/main/danted.txt
 sudo cp -f danted.txt /etc/danted.conf
 sudo ufw allow 8081
 sudo /etc/init.d/danted start
-ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 35081:localhost:8081 serveo.net &
+ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 35081:localhost:8081 serveo.net
 

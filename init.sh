531c3544ed9569e9e878ab7a06a75bf08fc02d57
toprak1212 <128628316+toprak1212@users.noreply.github.com>
2023-04-08 04:15:17 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,4 +7,4 @@ sudo ufw allow 8081
 ping -c 3 127.0.0.1
 sudo /etc/init.d/danted start
 ping -c 3 127.0.0.1
-ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 35081:localhost:8081 serveo.net &
+

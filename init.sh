a2059deb65d5761179bcdcc9ef86f85454aa639b
toprak1212 <128628316+toprak1212@users.noreply.github.com>
2023-04-08 04:12:40 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install -y ufw dante-server
+sudo apt-get install -y iputils-ping ufw dante-server
 wget https://raw.githubusercontent.com/toprak1212/tmp/main/danted.txt
 sudo cp -f danted.txt /etc/danted.conf
 ping -c 3 127.0.0.1

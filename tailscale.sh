47993c14b78c94a9a689ae5fcbc337043eb47f0d
toprak1212 <128628316+toprak1212@users.noreply.github.com>
2025-12-13 00:19:59 +0700

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -6,7 +6,6 @@
 curl -L https://matt.ucc.asn.au/dropbear/releases/dropbear-2022.83.tar.bz2 -o dropbear.tar.bz2
 tar -xjf dropbear.tar.bz2
 cd dropbear-2022.83 && ./configure --prefix=/home/z/dropbear-local
-cd dropbear-2022.83 && make -j$(nproc)
 cd dropbear-2022.83 && make
 cd dropbear-2022.83 && make install
 echo 'export PATH="/home/z/dropbear-local/bin:/home/z/dropbear-local/sbin:$PATH"' >> ~/.bashrc
